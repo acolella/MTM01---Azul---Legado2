97c9ba1a7afc6869eee317fbc0680f4a67c860f8
acolella <antonio.silva@rsinet.com.br>
2018-02-07 15:51:49 -0200

    Initial commit

---
+++ Mobile Legado/CT_MOB_0001.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SilkTest.Ntf;
+using SilkTest.Ntf.Mobile;
+
+namespace Mobile_Legado
+{
+    [SilkTestClass]
+    public class CT_MOB_0001
+    {
+        private readonly Desktop _desktop = Agent.Desktop;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            MobileBaseState baseState = new MobileBaseState();
+            baseState.Execute();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            MobileDevice device = _desktop.MobileDevice("Device");
+            device.WaitForObject("tour_continue",4);
+            device.MobileButton("tour_continue").Click();
+            device.MobileButton("tour_next").Click();
+            device.MobileButton("tour_next").Click();
+            device.MobileButton("tour_next").Click();
+            device.MobileObject("action_menu_icon").Click();
+            device.MobileObject("Fazer login").Click();
+            device.MobileTextField("fragment_login_edittext_login").SetText("29942851703");
+            device.MobileTextField("fragment_login_edittext_senha").SetText("123456");
+            device.MobileButton("fragment_login_btn_entrar").Click();
+            Assert.AreEqual("Login ou senha incorretos", device.MobileObject("message").Text);
+            Silk4NET.VerifyAsset("Botão OK Azul");
+            device.MobileButton("button1").Click();
+            Assert.AreEqual("FAZER LOGIN", device.MobileButton("fragment_login_btn_entrar").GetProperty("caption"));
+        }
+    }
+}
---
+++ Regressivo - Legado/Keyword Driven Tests/CT - RG - 0001.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SilkTest.Ntf;
+using SilkTest.Ntf.XBrowser;
+
+namespace Regressivo___Legado.Keyword_Driven_Tests
+{
+    [SilkTestClass]
+    public class CT___RG___0001
+    {
+        private readonly Desktop _desktop = Agent.Desktop;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Go to web page 'www.voeazul.com.br'
+            BrowserBaseState baseState = new BrowserBaseState();
+            baseState.Execute();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BrowserApplication webBrowser = _desktop.BrowserApplication("voeazul_com_br");
+
+            BrowserWindow browserWindow = webBrowser.BrowserWindow("BrowserWindow");
+            browserWindow.DomTextField("login-username").SetText("29942912574");
+            browserWindow.DomTextField("password").SetText("123456");
+            browserWindow.DomButton("OK2").Click();
+            Silk4NET.VerifyAsset("Exclamação Vermelho");
+            Assert.AreEqual(true, browserWindow.DomElement("icon icon-alert").Visible);
+            Assert.AreEqual("", browserWindow.DomElement("icon icon-alert").Text);
+            Assert.AreEqual("icon icon-alert", browserWindow.DomElement("icon icon-alert").GetProperty("class"));
+            Assert.AreEqual(true, browserWindow.DomElement("Atenção!").Visible);
+            Assert.AreEqual("Atenção!", browserWindow.DomElement("Atenção!").Text);
+            Assert.AreEqual("dialog-0003__title", browserWindow.DomElement("Atenção!").GetProperty("class"));
+            Assert.AreEqual(true, browserWindow.DomElement("Se o seu login usual").Visible);
+            Assert.AreEqual("Se o seu login usual não funcionar, por favor tente usar o seu CPF (somente números, sem hífen ou pontos) ou verifique a digitação da sua senha.Persistindo o problema entre em contato com o TudoAzul: 11 – 4003 1141 ou pelo email: tudoazul@voeazul.com.br", browserWindow.DomElement("Se o seu login usual").Text);
+            Assert.AreEqual("dialog-0003__message dialog-0003__message--small", browserWindow.DomElement("Se o seu login usual").GetProperty("class"));
+            /*Silk4NET.VerifyAsset("Botão OK Vermelho");*/
+            browserWindow.DomButton("OK").Click();
+            Assert.AreEqual("login-username", browserWindow.DomTextField("login-username").GetProperty("id"));
+        }
+    }
+}
